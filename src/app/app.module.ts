73822fb2fdffe620dc177c302b6d2b4ec3629014
ChrisBez <cjbezzina@gmail.com>
2019-02-20 17:55:53 +1000

    Routing to admin and user modes set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {MatButtonModule, MatCardModule, MatToolbarModule, MatSidenavModule, MatListModule, MatButtonToggleModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCardModule, MatToolbarModule, MatSidenavModule, MatListModule, MatButtonToggleModule} from '@angular/material';
+import { UserViewComponent } from './user-view/user-view.component';
+import { AdminViewComponent } from './admin-view/admin-view.component';
+
+const appRoutes: Routes = [
+  { path: 'user', component: UserViewComponent },
+  { path: 'admin', component: AdminViewComponent },
+  { path: '**', component: UserViewComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserViewComponent,
+    AdminViewComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoutes),
     BrowserAnimationsModule,
     MatToolbarModule,
     MatButtonModule
