6b71a7d7a60a7600f9ff3343f535630f0c825edb
Chris Bezzina <c.bezzina@mkysugar.com.au>
2019-02-22 11:06:38 +1000

    Changed Mock Cans to stock all to 5

--- src/app/cans/mock-cans.ts
+++ src/app/cans/mock-cans.ts
@@ -3,10 +3,10 @@ import { Can } from '../interfaces/can';
 export const CANS: Can[] = [
     {id: 1, flavour: 'Soda Water', price: .90, quantity: 5},
     {id: 2, flavour: 'Lemonade', price: 1.50, quantity: 5},
-    {id: 3, flavour: 'Cola', price: 1.50, quantity: 4},
+    {id: 3, flavour: 'Cola', price: 1.50, quantity: 5},
     {id: 4, flavour: 'Orange Juice', price: 1.50, quantity: 5},
-    {id: 5, flavour: 'Water', price: 1.50, quantity: 0},
-    {id: 6, flavour: 'Diet Cola', price: 1.15, quantity: 2},
+    {id: 5, flavour: 'Water', price: 1.50, quantity: 5},
+    {id: 6, flavour: 'Diet Cola', price: 1.15, quantity: 5},
     {id: 7, flavour: 'Ginger Ale', price: 1.50, quantity: 5},
     {id: 8, flavour: 'Ginger Beer', price: 1.40, quantity: 5},
 ];
